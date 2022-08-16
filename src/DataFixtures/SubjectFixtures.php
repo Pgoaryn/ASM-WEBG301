f1fda3036f68e23ca927e2d44c4bf62f1f1124bd
RizeElxtric <leminhvu809@gmail.com>
2022-08-16 10:13:43 +0700

    Adding missing subject Fixtures

--- src/DataFixtures/SubjectFixtures.php
+++ src/DataFixtures/SubjectFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Subject;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,7 +10,17 @@ class SubjectFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        
+        $names = ["IT", "Designer", "Digital Marketing"];
+        for ($i = 1; $i <= 15; $i++) {
+            $key = array_rand($names, 1);
+            $subject = new Subject;
+            $subject ->setName($names[$key])
+                    ->setInfo("Description of subject")
+                    ->setFee(rand(200,300));
+            $manager->persist($subject);
+        }
+
+        $manager->flush();
         $manager->flush();
     }
 }
