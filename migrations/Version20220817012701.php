2898b2fced2ff3e5a930c72c5ac498a4a69171ae
Pgoaryn <itszombie2016@gmail.com>
2022-08-17 09:08:23 +0700

    Subject html

--- migrations/Version20220817012701.php
+++ migrations/Version20220817012701.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220817002131 extends AbstractMigration
+final class Version20220817012701 extends AbstractMigration
 {
     public function getDescription(): string
     {
